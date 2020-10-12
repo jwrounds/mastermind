b3fe39b6b09d568f5d4caa595bdd304f09cdcb0a
Jason Rounds <jwrounds@usfca.edu>
2020-10-12 16:58:05 -0500

    fleshed out board display and player guess functionality

--- mastermind.rb
+++ mastermind.rb
@@ -23,13 +23,18 @@ class Board
   attr_accessor :turn, :key_pegs, :game_board
 
   def initialize 
-    @turn = 0
-    @key_pegs
+    @key_pegs = nil
     @game_board = [["*", "*", "*", "*"]]
   end
 
   def display 
-
+    self.game_board.each_with_index do |row, i|
+      displayed_row = "[#{row.join('--')}]"
+      if i > 0 && key_pegs
+        display_row += "[#{self.key_pegs[i]}]"
+      end
+      puts displayed_row
+    end
   end
 end
 
@@ -40,9 +45,18 @@ class Player
     @guess = []
   end
 
+  def make_guess board
+    puts (  
+      " \nGuess the sequence by entering a combination of four letters representing each color ('r' for red, 'g' for green, 't' for turquoise, 'y' for yellow, 'p' for purple, or 'o' for orange): \n"
+      )
+    new_guess = gets.chomp.upcase
+    self.guess = new_guess.split""
+    board.game_board.push self.guess
+  end
+
 end
 
-class Computer_player
+class ComputerPlayer
   attr_accessor :code
 
   def initialize colors
@@ -59,16 +73,26 @@ class Computer_player
 end
 
 class Game
-  attr_reader :colors. :board
+  attr_reader :colors, :board
+  attr_accessor :turn
 
   def initialize color_set, board
     @colors = color_set
     @board = board
+    @turn = 0
   end
 
+  new_board = Board.new
   def explain_rules
-    puts 
-      "You will have 10 turns to guess a random color sequence chosen by the computer. Colors can repeat. The available colors are #{self.colors.split(', ')} and each turn you will receive a white token for each matching color in the sequence. A black peg represents maching color and position.\n\nIf there are repeating colors in the sequence (ex. 'green, green, blue, blue') and your guess contains one of the correct colors out of sequence (ex. 'purple, red, orange, green'), you will receive only one white peg as feedback for that correctly guessed color. Likewise with a black peg if you guess one color out of miltiple repeats in the correct sequence (ex. 'green, orange, purple, red'). Type 'rules' to see this statement again."
+    puts (
+      " \nYou will have 10 turns to guess a random color sequence chosen by the computer. Colors can repeat. The available colors are #{self.colors.join(', ')} and each turn you will receive a white token for each matching color in the sequence. A black peg represents maching color and position.\n\nIf there are repeating colors in the sequence (ex. 'green, green, blue, blue') and your guess contains one of the correct colors out of sequence (ex. 'purple, red, orange, green'), you will receive only one white peg as feedback for that correctly guessed color. Likewise with a black peg if you guess one color out of miltiple repeats in the correct sequence (ex. 'green, orange, purple, red'). Type 'rules' to see this statement again.\n "
+      ) 
+  end
+
+  def start
+    self.explain_rules
+    @Computer_player = ComputerPlayer.new self.colors
+    self.board.display
   end
 end
 
